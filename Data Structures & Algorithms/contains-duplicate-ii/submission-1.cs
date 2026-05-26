44d1461f2ee15023551540189edaba9d2c7d5289
Tony Olori <anthonyolori123@gmail.com>
2026-05-26 22:26:10 +0100

    Add: contains-duplicate-ii - submission-1

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-1.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    //ai, need to resolve this
+    public bool ContainsNearbyDuplicate(int[] nums, int k) {
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = i + 1; j < nums.Length; j++) {
+                if (nums[i] == nums[j] && j - i <= k) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
